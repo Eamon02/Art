850be6cbb9470091c15b8f4449812c2b2c1fc87a
Eamon McHugh <eamonmchugh2@gmail.com>
2021-03-01 21:08:41 -0500

    fixed animation bug

--- grid.js
+++ grid.js
@@ -55,7 +55,7 @@
 //         });
 
 
-const text = new Letterize({
+const test = new Letterize({
     targets: ".sqr"
   });
 
@@ -66,22 +66,18 @@ scale: [
 {value: .1, easing: 'easeOutSine', duration: 500},
 {value: 1, easing: 'easeInOutQuad', duration: 1200}
 ],
-delay: anime.stagger(200, {grid: [text.list[0].length, text.list.length], from: 'center'}),
+delay: anime.stagger(200, {grid: [test.list[0].length, test.list.length], from: 'center'}),
 loop: false
 })};
 
 $(".sqr").on("click", animation);
-// $(window).on("load", animation);
+$(window).on("load", animation);
 $(".sqr").on("mouseover", scaleAnimation);
 
 
-function scaleAnimation() { anime.timeline({
-    targets: text.listAll,
+function scaleAnimation() { anime({
     scale: 0.5,
     letterSpacing: "10px",
-    .add({
-        
-    })
 })}
 
 // animation
